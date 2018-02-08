40a04b0f78d7bc6c03134ca8b7db7e5031a8f857
Billy O'Mahony <omahonybilly@gmail.com>
2018-02-08 11:35:57 +0000

    Created Inventory and Item

---
+++ Assets/Resources/Scripts/Inventory/AddToInventoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddToInventoryTest : MonoBehaviour {
+
+    public GameObject inventoryObj;
+    public GameObject itemObj;
+    public Inventory inventory;
+    public Item item;
+
+    private void GetInvAndItem()
+    {
+        this.inventory = inventoryObj.GetComponent<Inventory>();
+        this.item = itemObj.GetComponent<Item>();
+    }
+
+    public void ClickAdd()
+    {
+        GetInvAndItem();
+        inventory.AddToInventory(item);
+    }
+
+    public void ClickRemove()
+    {
+        GetInvAndItem();
+        inventory.RemoveFromInventory(item, 1);
+    }
+
+}
---
+++ Assets/Resources/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    // This inventory
+    Dictionary<Item, int> inventory = new Dictionary<Item, int>();
+
+
+    // Adds item to inventory
+	public void AddToInventory(Item item)
+    {
+        if (inventory.ContainsKey(item))
+        {
+            Debug.Log(gameObject.name + ": " + item.itemName + " already exists. Incrementing.");
+            inventory[item]++;
+        }
+        else
+        {
+            Debug.Log(gameObject.name + ": Adding " + item.itemName);
+            inventory.Add(item, 1);
+        }
+    }
+
+    public void RemoveFromInventory(Item item, int number)
+    {
+        if (inventory.ContainsKey(item))
+        {
+            if (inventory[item] > number)
+            {
+                Debug.Log(gameObject.name + ": Removing " + number + " " + item.itemName + "s.");
+                inventory[item] -= number;
+            }
+            else if (inventory[item] == number)
+            {
+                Debug.Log(gameObject.name + ": Removing " + item.itemName + " from inventory");
+                inventory.Remove(item);
+            }
+            else
+            {
+                Debug.Log(gameObject.name + ": Trying to remove too many " + item.itemName + "s");
+            }
+        }
+        else
+        {
+            Debug.Log(gameObject.name + ": Removing " + item.itemName + " is not in dictionary, cannot be removed.");
+        }
+    }
+
+
+}
