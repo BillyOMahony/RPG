f0b7d6ac4c3409fb0da059d13c41fbb9a37e4c9a
Billy O'Mahony <omahonybilly@gmail.com>
2018-03-07 15:10:32 +0000

    Created some random scripts
    FloatingObject uses sin to make give a floating effect to an object.
    InteractableAddForce implements Interactable. When interacted with it adds a force in a direction to the its gameObject.

---
+++ Assets/Resources/Scripts/Interactable/Random/InteractableAddForce.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableAddForce : Interactable {
+
+    private Rigidbody rb;
+
+    public Vector3 ForceDirection = new Vector3(0, 1, 0);
+    public float ForceMultiplier = 100f;
+    public float RandomOffsetMaximum = 0.1f; 
+
+    public override void Interact()
+    {
+        Vector3 offset = GetRandomDirectionOffset(RandomOffsetMaximum);
+        rb.AddForce((ForceDirection + offset) * ForceMultiplier);  
+    }
+
+    // Use this for initialization
+    void Start () {
+        Initialize();
+        if ((rb = GetComponent<Rigidbody>()) == null)
+        {
+            rb = gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
+        }
+    }
+	
+	Vector3 GetRandomDirectionOffset(float OffsetAmount)
+    {
+        return new Vector3(Random.Range(OffsetAmount * -1, OffsetAmount),
+                Random.Range(OffsetAmount * -1, OffsetAmount),
+                Random.Range(OffsetAmount * -1, OffsetAmount)
+            );
+    }
+
+
+}
---
+++ Assets/Resources/Scripts/Misc/ObjectEffects/FloatingObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingObject : MonoBehaviour {
+
+    public float Multiplier = 1;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, Mathf.Sin(Time.time) * Multiplier, 0);
+	}
+}
