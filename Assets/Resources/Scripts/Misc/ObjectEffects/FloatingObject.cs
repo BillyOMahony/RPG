8e259c3bf76a2deb78c77a6423b376f69ddeb776
Billy O'Mahony <omahonybilly@gmail.com>
2018-03-22 13:07:53 +0000

    Changed FloatingObject script

--- Assets/Resources/Scripts/Misc/ObjectEffects/FloatingObject.cs
+++ Assets/Resources/Scripts/Misc/ObjectEffects/FloatingObject.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class FloatingObject : MonoBehaviour {
 
     public float Multiplier = 1;
+    public float Speed = 20;
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(0, Mathf.Sin(Time.time) * Multiplier, 0);
-	}
+        transform.Translate(transform.forward * (Speed * Time.deltaTime));
+        transform.Translate(transform.up * Mathf.Sin(Time.time) * Multiplier);
+        transform.Translate(transform.right * Mathf.Cos(Time.time) * Multiplier);
+    }
 }
